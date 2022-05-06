190561aa34dee757e3315e5c42741823daf594fb
Matheus Portella Demarchi <matheus_portella_demarchi@hotmail.com>
2022-05-06 17:19:20 -0300

    06/05

--- CleanArch/Gcsb.Connect.CleanArch.WebAPI/Program.cs
+++ CleanArch/Gcsb.Connect.CleanArch.WebAPI/Program.cs
@@ -1,25 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using Autofac.Extensions.DependencyInjection;
+using Gcsb.Connect.Pkg.Serilog.Implementation;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Gcsb.Connect.Fines.Webapi
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool logEnable = Environment.GetEnvironmentVariable("POSTGRES_LOG_CONN") != null;
+
+            Log.Logger = new Logger().ConfigureLogger(logEnable);
+            Log.Information("Starting web host");
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
