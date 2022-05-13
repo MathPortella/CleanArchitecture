521ca9bf41f92e64b6fcff46cd386a170eb86038
Matheus Portella Demarchi <matheus_portella_demarchi@hotmail.com>
2022-05-13 09:41:19 -0300

    13/05

---
+++ CleanArch/Gcsb.Connect.CleanArch.Domain/Entities/Customer.cs
@@ -0,0 +1,36 @@
+﻿using Gcsb.Connect.CleanArch.Domain.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gcsb.Connect.CleanArch.Domain.Entities
+{
+    public class Customer : Entity
+    {
+        public string Name { get; private set; }
+        public string BirthDate { get; private set; }
+        public string Rg { get; private set; }
+        public string Cpf { get; private set; }
+        public string Address { get; private set; }
+        public int PostalCode { get; private set; }
+        public DateTime RegistrationDate { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public Customer(string name, string birthDate, string rg, string cpf, string address, int postalCode)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            BirthDate = birthDate;
+            Rg = rg;
+            Cpf = cpf;
+            Address = address;
+            PostalCode = postalCode;
+            RegistrationDate = DateTime.UtcNow;
+            IsActive = true;
+
+            Validate(this, new CustomerValidator());
+        }
+    }
+}
---
+++ CleanArch/Gcsb.Connect.CleanArch.Domain/Validator/CustomerValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Gcsb.Connect.CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gcsb.Connect.CleanArch.Domain.Validator
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(r => r.Id).NotEmpty().NotEqual(new Guid());
+            RuleFor(r => r.Name).NotEmpty().NotNull().MaximumLength(50);
+            RuleFor(r => r.BirthDate).NotEmpty().NotNull().MaximumLength(10);
+            RuleFor(r => r.Rg).NotEmpty().NotNull().MaximumLength(12);
+            RuleFor(r => r.Cpf).NotEmpty().NotNull().MaximumLength(11);
+            RuleFor(r => r.Address).NotEmpty().NotNull().MaximumLength(100);
+            RuleFor(r => r.PostalCode).NotEmpty().NotNull();
+        }
+    }
+}
